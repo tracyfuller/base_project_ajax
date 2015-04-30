17bdc8d72b1f14a85a6b0d9e60b22a8426eabfc4
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-30 17:37:10 -0500

    Index created, data.json created, script created, template created. Init button activated, remove button activated

---
+++ script.js
@@ -0,0 +1,36 @@
+var cityHtml = null;
+var cityData = null;
+
+$(document).ready(function(){
+    console.log(cityHtml, cityData);
+
+    $("#initiateBtn").on('click', function(){
+        $.get("template.html", function(data){
+            if (cityHtml === null){
+                cityHtml = data;
+                $("#dataBox").append(cityHtml);
+                console.log("got it", cityHtml);
+            } else {
+                console.log("you already have city html");
+            }
+        });
+        $.get('data.json', function(data){
+           if (cityData === null){
+               cityData = data;
+               $('#name').append("Name: " + cityData.location[0].name);
+               $('#area').append("Area: " + cityData.location[0].area);
+               $('#population').append("Population: " + cityData.location[0].population);
+               console.log("got data", cityData);
+           } else {
+               console.log("already have city data");
+           }
+        });
+    });
+
+    $('#dataBox').on('click', '#removeBtn', function(){
+        $(this).parent().remove();
+    });
+
+
+
+});
